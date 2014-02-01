0205b56989d201e17810b43539b29c8bc0aba31e
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-01-31 19:00:36 -0800

    Update template for rules

--- rule/templates/_test.js
+++ rule/templates/_test.js
@@ -8,13 +8,13 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var eslintTester = require("../../../lib/tests/eslintTester");
+var eslintTester = require("eslint-tester");
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-eslintTester.addRuleTest("<%= ruleId %>", {
+eslintTester.addRuleTest("lib/rules/<%= ruleId %>", {
 
     valid: [
 
