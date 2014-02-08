06289a34b11717fe79d0b819bdae6d84f410d43c
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-02-08 11:01:09 -0800

    Update yeoman-generator

--- test/test-load.js
+++ test/test-load.js
@@ -5,7 +5,7 @@ var assert = require('assert');
 
 describe('eslint generator', function () {
     it('can be imported without blowing up', function () {
-        var app = require('../app');
+        var app = require('../rule');
         assert(app !== undefined);
     });
 });
