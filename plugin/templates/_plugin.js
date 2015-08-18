c8c0335419702c5c49d5c86d461e0eee75c04330
Nicholas C. Zakas <nicholas@nczconsulting.com>
2015-08-18 14:41:41 -0700

    New: Plugin generator (fixes #2)

---
+++ plugin/templates/_plugin.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview <%= desc %>
+ * @author <%= userName %>
+ * @copyright <%= year%> <%= userName %>. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var requireIndex = require("requireindex");
+
+//------------------------------------------------------------------------------
+// Plugin Definition
+//------------------------------------------------------------------------------
+
+<% if (hasRules) { %>
+// import all rules in lib/rules
+module.exports.rules = requireIndex("./rules");
+<% } %>
+
+<% if (hasProcessors) { %>
+// import processors
+module.exports.processors = {
+
+    // add your processors here
+};
+<% } %>
