671329ef258bdd663a3138a578ee0d754da4b7d6
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2021-07-22 17:40:21 -0700

    Update: Fix linting inconsistencies in template (#106)

--- rule/templates/_test.js
+++ rule/templates/_test.js
@@ -8,32 +8,26 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/<%= ruleId %>"),
-<% if (target === "eslint") { %>
-    RuleTester = require("../../../lib/testers/rule-tester");
+const rule = require("../../../lib/rules/<%= ruleId %>"),<% if (target === "eslint") { %>
+  RuleTester = require("../../../lib/testers/rule-tester");
 <% } else { %>
-    RuleTester = require("eslint").RuleTester;
+  RuleTester = require("eslint").RuleTester;
 <% } %>
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
+const ruleTester = new RuleTester();
 ruleTester.run("<%= ruleId %>", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+  ],
 
-    valid: [
-
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "<%- invalidCode.replace(/"/g, '\\"') %>",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
+  invalid: [
+    {
+      code: "<%- invalidCode.replace(/"/g, '\\"') %>",
+      errors: [{ message: "Fill me in.", type: "Me too" }],
+    },
+  ],
 });
