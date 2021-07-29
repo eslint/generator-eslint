11555c4c66e6ef4fa9cb1ede00949526c247c2da
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2021-07-29 12:26:59 -0700

    Update tests/test-load.js
    Co-authored-by: Brandon Mills <btmills@users.noreply.github.com>

--- tests/test-load.js
+++ tests/test-load.js
@@ -6,7 +6,8 @@
 // eslint-disable-next-line node/no-missing-import -- https://github.com/mysticatea/eslint-plugin-node/issues/275
 import assert from "node:assert";
 
-const importFresh = async modulePath => import(`${modulePath}?x=${new Date()}`); // eslint-disable-line func-style, node/no-unsupported-features/es-syntax -- https://github.com/mysticatea/eslint-plugin-node/issues/250
+// eslint-disable-next-line func-style, node/no-unsupported-features/es-syntax -- https://github.com/mysticatea/eslint-plugin-node/issues/250
+const importFresh = async modulePath => import(`${modulePath}?x=${new Date()}`);
 
 describe("eslint generator", () => {
     it("can be imported without blowing up", () => {
