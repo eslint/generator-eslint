ed00e494a3444de515a6bc1421390207d515e990
唯然 <weiran.zsd@outlook.com>
2024-05-21 14:51:44 +0800

    fix: use 'flat/mixed-esm-and-cjs'

--- plugin/templates/_eslint.config.mjs
+++ plugin/templates/_eslint.config.mjs
@@ -4,6 +4,6 @@ import eslintPlugin from "eslint-plugin-eslint-plugin";
 
 export default [
     pluginJs.configs.recommended,
-    pluginNode.configs["flat/recommended"],
+    ...pluginNode.configs["flat/mixed-esm-and-cjs"],
     eslintPlugin.configs["flat/recommended"]
 ];
