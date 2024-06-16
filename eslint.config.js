3a70aac6f46626926b058d8b2a90e35f8b084560
Milos Djermanovic <milos.djermanovic@gmail.com>
2024-06-16 20:36:11 +0200

    chore: switch to eslint-config-eslint v11

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import globals from "globals";
 import eslintConfigESLint from "eslint-config-eslint";
+import eslintConfigESLintFormatting from "eslint-config-eslint/formatting";
 
 export default [
     ...eslintConfigESLint,
+    eslintConfigESLintFormatting,
     {
         name: "generator-eslint/global-ignores",
         ignores: ["temp/", "*/templates/*"]
