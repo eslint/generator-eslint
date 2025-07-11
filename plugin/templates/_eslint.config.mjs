e75eda9e42c68c9f1b74a37eb65fbd4aa28c7723
루밀LuMir <rpfos@naver.com>
2025-07-11 22:22:49 +0900

    wip: update `eslint.config.mjs`

--- plugin/templates/_eslint.config.mjs
+++ plugin/templates/_eslint.config.mjs
@@ -4,7 +4,25 @@ import pluginNode from "eslint-plugin-n";
 import eslintPlugin from "eslint-plugin-eslint-plugin";
 
 export default defineConfig([
-    pluginJs.configs.recommended,
-    pluginNode.configs["flat/mixed-esm-and-cjs"],
-    eslintPlugin.configs["flat/recommended"]
+    {
+        name: "eslint/js",
+        plugins: {
+            js: pluginJs,
+        },
+        extends: ["js/recommended"],
+    },
+    {
+        name: "eslint/node",
+        plugins: {
+            n: pluginNode,
+        },
+        extends: ["n/flat/mixed-esm-and-cjs"],
+    },
+    {
+        name: "eslint/eslint-plugin",
+        plugins: {
+            "eslint-plugin": eslintPlugin,
+        },
+        extends: ["eslint-plugin/flat/recommended"],
+    }
 ]);
